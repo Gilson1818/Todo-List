cd03203c26ea046dac9049d61e2398eda8d0bed7
unknown <carvallhh#gmail.com>
2023-03-25 17:29:32 -0300

    first commit

---
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-itens',
+  templateUrl: './todo-input-add-itens.component.html',
+  styleUrls: ['./todo-input-add-itens.component.scss']
+})
+export class TodoInputAddItensComponent implements OnInit {
+
+  @Output() public EmitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = "";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  public submitItemTaskList(){
+    this.addItemTaskList = this.addItemTaskList.trim();
+    
+    if(this.addItemTaskList){
+      this.EmitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+    
+  }
+}
